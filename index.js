c481822faf2c224a46d9b09c1cca8b202dcca939
tdev88 <tracy.dev@outlook.com>
2021-07-14 13:59:54 +1000

    update

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code in this file!
+let currentUser = ('string');
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+const welcomeMessage = (`Welcome to Flatbook, ${currentUser} !`);
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = (`Welcome, ${firstInitial} !`);
